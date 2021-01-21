82d720aeb17c7b6b7de7bb56b67d4649b7731aca
digitalprojections <fuzalov@gmail.com>
2021-01-21 14:38:43 +0900

    Create open_ayah.js
    adding open ayah by parameter

---
+++ js/open_ayah.js
@@ -0,0 +1,32 @@
+/* 
+ * Author Ahmad - sn engineer, teacher and content creator
+ * Date: 2021 January 21 
+ */
+
+var searchString = window.location.search.replace("?","");
+var searchArgs = getParameters(searchString)
+
+function getParameters(s){
+    var s_one = "";
+    var s_two = "";
+    if(s!=undefined){
+        if(s.indexOf("&")>0){
+            var s_ar = s.split("&");
+            if(s_ar.length>1){
+                //ideally, this should work as the url is constructed in my android app
+                //the way I expect
+                s_one = s_ar[0].split("=")[1];
+                s_two = s_ar[1].split("=")[1];
+                console.log(s_ar);
+            }else{
+                //we may never hit this
+            }
+        }else{
+            //parameter pattern doesn`t match
+        }
+    }else{
+        //string is NULL
+    }    
+    return new Array(s_one, s_two);
+}
+
